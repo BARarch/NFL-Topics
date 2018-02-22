00bcca08b41d6d5e7491db8635baab8a485306ff
Anthony Quivers <anthonyquivers@gmail.com>
2018-02-22 18:11:10 -0500

    Adjusted for new RSS LIST

--- NFL-Parse-2-0/pullFeeds.py
+++ NFL-Parse-2-0/pullFeeds.py
@@ -18,7 +18,7 @@ import corpus as cp
 def getFeeds():
     """Google Sheets API Code.
     Pulls urls for all NFL Team RSS Feeds
-    https://docs.google.com/spreadsheets/d/1DUBb9OG1A1Xs6v2PK9Oislz4384DSu2MzEbH8dK0ad4/edit#gid=0
+    https://docs.google.com/spreadsheets/d/1XiOZWw3S__3l20Fo0LzpMmnro9NYDulJtMko09KsZJQ/edit#gid=0
     """
     credentials = get_credentials()
     http = credentials.authorize(mgs.httplib2.Http())
@@ -28,8 +28,8 @@ def getFeeds():
                               discoveryServiceUrl=discoveryUrl)
 
     #specify sheetID and range
-    spreadsheetId = '1DUBb9OG1A1Xs6v2PK9Oislz4384DSu2MzEbH8dK0ad4'
-    rangeName = 'Sheet1!A2:E'
+    spreadsheetId = '1XiOZWw3S__3l20Fo0LzpMmnro9NYDulJtMko09KsZJQ'
+    rangeName = 'RSS LIST!A2:E'
     result = service.spreadsheets().values().get(
         spreadsheetId=spreadsheetId, range=rangeName).execute()
     values = result.get('values', [])
