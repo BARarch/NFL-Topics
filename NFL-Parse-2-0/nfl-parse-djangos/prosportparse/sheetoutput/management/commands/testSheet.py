f5e3dfa8bad6c76f7073ad2a087d747eba13fe44
Anthony Quivers <anthonyquivers@gmail.com>
2018-06-16 23:09:08 -0400

    Make it a view

--- NFL-Parse-2-0/nfl-parse-djangos/prosportparse/sheetoutput/management/commands/testSheet.py
+++ NFL-Parse-2-0/nfl-parse-djangos/prosportparse/sheetoutput/management/commands/testSheet.py
@@ -4,6 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 from parsebot.models import Article
 from sheetoutput.models import CredetialsModel
 
+from oauth2client.contrib import xsrfutil
 from oauth2client.contrib.django_util.storage import DjangoORMStorage
 from oauth2client.client import flow_from_clientsecrets
 
@@ -21,7 +22,7 @@ class Command(BaseCommand):
 		CLIENT_SECRET_FILE = settings.GOOGLE_OAUTH2_CLIENT_SECRETS_JSON
 		APPLICATION_NAME = 'NFL-Parse'
 
-		flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=SCOPES)
+		flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=SCOPES, redirect_uri='http://localhost:8080/')
 		print('got flow')
 
 		storage = DjangoORMStorage(	CredetialsModel, 
@@ -35,9 +36,13 @@ class Command(BaseCommand):
 		print(storage)
 
 		if cred is None or credential.invalid == True:
+			flow.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
+															0)
 			print('we have not valid credential stored')
 			print('we are using flow to generate token')
-
+			print(flow.params)
+			authorize_url = flow.step1_get_authorize_url()
+			print('authorize_url: {}'.format(str(authorize_url)))
 			## If the flow is bad we might have to go to Google developers console
 			## and get a new server to server secret for this django.
 		else:
